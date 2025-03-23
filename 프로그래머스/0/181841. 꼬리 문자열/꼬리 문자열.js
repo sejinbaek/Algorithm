4777a969c877a9700a9b6a355b8d4fb50e753478
sejin_Baek <117803382+sejinbaek@users.noreply.github.com>
2025-03-23 15:34:57 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,11 +1,4 @@
 function solution(str_list, ex) {
-    var answer = '';
-    answer = str_list.join();
-    for(let i = 0; i < str_list.length; i++) {
-        if(str_list[i].includes(ex)) {
-            delete str_list[i];
-        }
-    }
-    answer = str_list.join("");
-    return answer;
+    const filtered = str_list.filter(str => !str.includes(ex));
+    return filtered.join('');
 }
