9fb8d89755b3bc44b6678bcc6bef736d89f89af6
sejin_Baek <117803382+sejinbaek@users.noreply.github.com>
2025-03-29 21:42:55 +0900

    [level 2] Title: 올바른 괄호, Time: 20.33 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 54.6 MB, 시간: 17.49 ms
+메모리: 54.9 MB, 시간: 20.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 22:42:07
+2025년 03월 29일 21:42:50
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,17 @@
-import java.util.*;
-
+import java.util.Stack;
 class Solution {
     boolean solution(String s) {
-        
         Stack<Character> stack = new Stack<>();
-        
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') { //'('이면 stack에 넣기
-                stack.push(s.charAt(i)); 
-            } else { //')'인데
-                if(stack.isEmpty()) { //stack이 비어있다면
-                    return false;
-                } 
-                stack.pop();
+            char parentheses = s.charAt(i);
+            if(parentheses == '(') {
+                stack.push(parentheses);
+            } else { // ')'라면
+                if(!stack.isEmpty()) stack.pop();
+                else return false;
             }
         }
-        
-        if(stack.isEmpty()) {
-            return true;
-        } else {
-            return false;
-        }
 
+        return stack.isEmpty();
     }
 }
