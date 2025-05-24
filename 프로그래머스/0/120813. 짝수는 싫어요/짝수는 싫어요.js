441c6147379d3dd4911686fff3a398bbff6742a8
sejin_Baek <117803382+sejinbaek@users.noreply.github.com>
2025-05-24 21:15:12 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,3 +1,3 @@
 function solution(n) {
-    return Array.from(new Array(n), (x, i) => i + 1).filter(i => i % 2 == 1).sort((a, b) => a - b);
+    return Array.from(new Array(n), (_, i) => i + 1).filter(i => i % 2 == 1);
 }
