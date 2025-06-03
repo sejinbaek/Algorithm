0371ac26b63c3b1cf45cce3696a249ba1053137b
sejin_Baek <117803382+sejinbaek@users.noreply.github.com>
2025-06-03 15:25:46 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 24.03 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    let answer = 0;
+    const min = Math.min(a, b)
+    const max = Math.max(a, b)
+    
+    for(let i = min; i <= max; i++) {
+        answer += i;
+    }
+    return answer;
+}
